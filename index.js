40591527658dfcfa9ebf76f245be7b1503619e9b
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-07-08 15:03:44 +0100

    MVP completes

--- index.js
+++ index.js
@@ -23,30 +23,65 @@ server.get('/api/users/:id', (req, res) => {
     const { id } =req.params;
     User.findById(Number(id))
     .then(data => {
-       if(data.length === 0 ) {
-           res.status(404).json({ message: 'users  id not found' })
+       if(Object.keys(data).length === 0 ) {
+           return res.status(404).json({ message: 'users  id not found' })
        }
         res.status(200).json(data);
     })
     .catch(error => {
-        res.status(500).json({ error: "The users information could not be retrieved." });
+        return res.status(500).json({ error: "The users information could not be retrieved." });
     })
 })
 
 server.post('/api/users', (req, res) => {
-    console.log('cjdh');
+    const {name, bio } =req.body;
+    const user = {
+        name, bio
+    }
+    if(typeof user.name === 'undefined' || typeof user.bio === 'undefined' ) {
+        return res.status(404).json({ errorMessage: "Please provide name and bio for the new user." })
+    }
+    User.insert(user)
+
+    .then(data => {
+         return res.status(201).json({ message: 'user  created successfully', data: user } );
+     })
+     .catch(error => {
+         return res.status(500).json({ error: "The users information could not be created." });
+     })
 })
 
 
-server.put('/api/users', (req, res) => {
-    console.log('cjdhcdjv');
+server.put('/api/users/:id', (req, res) => {
+    const { id } =req.params;
+    const {name, bio } =req.body;
+    const user = {
+        name, bio
+    }
+    User.update(Number(id), user )
+    .then(data => {
+        res.status(200).json({ message: 'user updated successfully', data: user });
+    })
+    .catch(error => {
+        return res.status(500).json({ error: "The users information could not be retrieved." });
+    })
 })
 
-server.delete('/api/users', (req, res) => {
-    console.log('cjdhcdjv');
+server.delete('/api/users/:id', (req, res) => {
+    const { id } =req.params;
+    User.remove(Number(id))
+    .then(data => {
+        if(!data) {
+            return res.status(404).json({ message: `The user with the ${id} does not exist.`});
+        }
+        res.status(200).json({ message: `The user with the ${id} has been removed`});
+    })
+    .catch(error => {
+        return res.status(500).json({ error: "The user could not be removed" });
+    })
 })
 
 
 server.listen(3000, () => {
-    console.log('Server listening on 5000')
+    console.log('Server listening on 3000')
 });
